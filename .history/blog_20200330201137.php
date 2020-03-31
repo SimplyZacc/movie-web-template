822d2fd18b3cd0ba4caf5904654e5bebfec8a016
SimplyZacc <Zacariewalkes@hotmail.com>
2020-03-30 21:25:00 -0400

    Blog page finished but needs styling

---
+++ .history/blog_20200330201137.php
@@ -0,0 +1,23 @@
+<?php
+//set page vars
+$title = "Blog";
+//include page header
+include "templates/header.php";
+?>
+
+<div class="contact-head">
+    <div class="contact-head-body">
+        <h2>Blog</h2>
+        <hr>
+        <p>Whats new with Theatre.</p>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-3">
+        </div>
+    </div>
+</div>
+
+<?php include "templates/footer.php"; ?>
---
+++ .history/blog_20200330201218.php
@@ -0,0 +1,63 @@
+<?php
+//set page vars
+$title = "Blog";
+//include page header
+include "templates/header.php";
+?>
+
+<div class="contact-head">
+    <div class="contact-head-body">
+        <h2>Blog</h2>
+        <hr>
+        <p>Whats new with Theatre.</p>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4">
+        a
+        </div>
+        <div class="col-sm-4">
+        a
+        </div>
+        <div class="col-sm-4">
+        a
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-4">
+        a
+        </div>
+        <div class="col-sm-4">
+        a
+        </div>
+        <div class="col-sm-4">
+        a
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-4">
+        a
+        </div>
+        <div class="col-sm-4">
+        a
+        </div>
+        <div class="col-sm-4">
+        a
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-4">
+        a
+        </div>
+        <div class="col-sm-4">
+        a
+        </div>
+        <div class="col-sm-4">
+        a
+        </div>
+    </div>
+</div>
+
+<?php include "templates/footer.php"; ?>
