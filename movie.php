f978844c14fc6a549f39043b6219163419a9c498
SimplyZacc <Zacariewalkes@hotmail.com>
2020-04-03 19:57:40 -0400

    Revert "Merge pull request #3 from SimplyZacc/movie"
    This reverts commit f15129ee7e8d72044fca3e4316c9583ecf3a8377, reversing
    changes made to 2862a52e378194e618e17c91a12135fb205e17d2.

--- movie.php
+++ movie.php
@@ -8,7 +8,7 @@ include "templates/header.php";
     <div class="contact-head-body">
         <h2>Movies</h2>
         <hr>
-        <p>Current movies showing sorted by popularity.</p>
+        <p>Current movies showing.</p>
     </div>
 </div>
 
@@ -31,12 +31,12 @@ include "templates/header.php";
                                     </h4>
                                 </div>
                                 <div class="movie-info">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum tempus egestas sed sed risus pretium quam vulputate. Ultricies mi quis hendrerit dolor magna. Amet commodo nulla facilisi nullam vehicula ipsum a arcu.
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum tempus egestas sed sed risus pretium quam vulputate. Ultricies mi quis hendrerit dolor magna. Amet commodo nulla facilisi nullam vehicula ipsum a arcu. Lorem ipsum dolor sit amet consectetur adipiscing. Mauris commodo quis imperdiet massa tincidunt nunc pulvinar. Elementum facilisis leo vel fringilla est. Ut porttitor leo a diam sollicitudin tempor id eu nisl. Pretium fusce id velit ut tortor pretium viverra suspendisse potenti. Ut consequat semper viverra nam libero justo.
                                 </div>
                                 <div class="movie-info-bottom">
                                     <span style="color: gold"><i class="fas fa-star"></i> <i class="fas fa-star"></i> <i class="fas fa-star"></i> <i class="far fa-star"></i> <i class="far fa-star"></i></span>
                                     <hr>
-                                    <span class="blog-info"><i class="far fa-user"></i> Admin <span style="padding-left: 10px">&nbsp;</span> <i class="far fa-calendar-alt"></i> March 30, 2020 <i class="far fa-eye"></i> 100</span>
+                                    <span class="blog-info"><i class="far fa-user"></i> Admin <span style="padding-left: 10px">&nbsp;</span> <i class="far fa-calendar-alt"></i> March 30, 2020</span>
                                 </div>
                             </div>
                         </div>
@@ -48,121 +48,15 @@ include "templates/header.php";
         <div class="col-sm-12">
             <div class="movie-border">
                 <div class="movie-inside">
-                    <div class="row">
-                        <div class="col-sm-4">
-                            <div class="movie-img">
-                                <img src="assets/images/photo-manipulation-1825450.jpg" style="width: 100%; height: 100%;" alt="">
-                            </div>
-                        </div>
-                        <div class="col-sm-8">
-                            <div class="movie-txt">
-                                <div class="movie-header">
-                                    <h4>
-                                        A new alien movie
-                                    </h4>
-                                </div>
-                                <div class="movie-info">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum tempus egestas sed sed risus pretium quam vulputate. Ultricies mi quis hendrerit dolor magna. Amet commodo nulla facilisi nullam vehicula ipsum a arcu.
-                                </div>
-                                <div class="movie-info-bottom">
-                                    <span style="color: gold"><i class="fas fa-star"></i> <i class="fas fa-star"></i> <i class="fas fa-star"></i> <i class="far fa-star"></i> <i class="far fa-star"></i></span>
-                                    <hr>
-                                    <span class="blog-info"><i class="far fa-user"></i> Admin <span style="padding-left: 10px">&nbsp;</span> <i class="far fa-calendar-alt"></i> March 30, 2020 <i class="far fa-eye"></i> 95</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+                    <div class="movie-img">
 
-        <div class="col-sm-12">
-            <div class="movie-border">
-                <div class="movie-inside">
-                    <div class="row">
-                        <div class="col-sm-4">
-                            <div class="movie-img">
-                                <img src="assets/images/photo-manipulation-1825450.jpg" style="width: 100%; height: 100%;" alt="">
-                            </div>
-                        </div>
-                        <div class="col-sm-8">
-                            <div class="movie-txt">
-                                <div class="movie-header">
-                                    <h4>
-                                        A new alien movie
-                                    </h4>
-                                </div>
-                                <div class="movie-info">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum tempus egestas sed sed risus pretium quam vulputate. Ultricies mi quis hendrerit dolor magna. Amet commodo nulla facilisi nullam vehicula ipsum a arcu.
-                                </div>
-                                <div class="movie-info-bottom">
-                                    <span style="color: gold"><i class="fas fa-star"></i> <i class="fas fa-star"></i> <i class="fas fa-star"></i> <i class="far fa-star"></i> <i class="far fa-star"></i></span>
-                                    <hr>
-                                    <span class="blog-info"><i class="far fa-user"></i> Admin <span style="padding-left: 10px">&nbsp;</span> <i class="far fa-calendar-alt"></i> March 30, 2020 <i class="far fa-eye"></i> 70</span>
-                                </div>
-                            </div>
-                        </div>
                     </div>
-                </div>
-            </div>
-        </div>
+                    <div class="movie-txt">
+                        <div class="movie-header">
 
-        <div class="col-sm-12">
-            <div class="movie-border">
-                <div class="movie-inside">
-                    <div class="row">
-                        <div class="col-sm-4">
-                            <div class="movie-img">
-                                <img src="assets/images/photo-manipulation-1825450.jpg" style="width: 100%; height: 100%;" alt="">
-                            </div>
                         </div>
-                        <div class="col-sm-8">
-                            <div class="movie-txt">
-                                <div class="movie-header">
-                                    <h4>
-                                        A new alien movie
-                                    </h4>
-                                </div>
-                                <div class="movie-info">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum tempus egestas sed sed risus pretium quam vulputate. Ultricies mi quis hendrerit dolor magna. Amet commodo nulla facilisi nullam vehicula ipsum a arcu.
-                                </div>
-                                <div class="movie-info-bottom">
-                                    <span style="color: gold"><i class="fas fa-star"></i> <i class="fas fa-star"></i> <i class="fas fa-star"></i> <i class="far fa-star"></i> <i class="far fa-star"></i></span>
-                                    <hr>
-                                    <span class="blog-info"><i class="far fa-user"></i> Admin <span style="padding-left: 10px">&nbsp;</span> <i class="far fa-calendar-alt"></i> March 30, 2020 <i class="far fa-eye"></i> 60</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+                        <div class="movie-info">
 
-        <div class="col-sm-12">
-            <div class="movie-border">
-                <div class="movie-inside">
-                    <div class="row">
-                        <div class="col-sm-4">
-                            <div class="movie-img">
-                                <img src="assets/images/photo-manipulation-1825450.jpg" style="width: 100%; height: 100%;" alt="">
-                            </div>
-                        </div>
-                        <div class="col-sm-8">
-                            <div class="movie-txt">
-                                <div class="movie-header">
-                                    <h4>
-                                        A new alien movie
-                                    </h4>
-                                </div>
-                                <div class="movie-info">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum tempus egestas sed sed risus pretium quam vulputate. Ultricies mi quis hendrerit dolor magna. Amet commodo nulla facilisi nullam vehicula ipsum a arcu.
-                                </div>
-                                <div class="movie-info-bottom">
-                                    <span style="color: gold"><i class="fas fa-star"></i> <i class="fas fa-star"></i> <i class="fas fa-star"></i> <i class="far fa-star"></i> <i class="far fa-star"></i></span>
-                                    <hr>
-                                    <span class="blog-info"><i class="far fa-user"></i> Admin <span style="padding-left: 10px">&nbsp;</span> <i class="far fa-calendar-alt"></i> March 30, 2020 <i class="far fa-eye"></i> 50</span>
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
